5edd9af4fa32e7c0d4a50b14af52757392480208
Benedikt Bauer <benedikt.bauer@posteo.de>
2022-05-28 17:20:25 +0200

    adjusted sim post command

--- sim/ghdl/counter/vhdl/Manifest.py
+++ sim/ghdl/counter/vhdl/Manifest.py
@@ -2,7 +2,7 @@ action = "simulation"
 sim_tool = "ghdl"
 sim_top = "counter_tb"
 
-sim_post_cmd = "ghdl -r counter_tb --stop-time=6us --vcd=counter_tb.vcd; gtkwave counter_tb.vcd"
+sim_post_cmd = "ghdl -r counter_testbench --stop-time=6us --vcd=counter_testbench.vcd; gtkwave counter_testbench.vcd"
 
 modules = {
     "local": ["../../../../testbench/counter_testbench"]
