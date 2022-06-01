006c421e83b8d5acd17afc62ab31cd01db494fd5
Benedikt Bauer <benedikt.bauer@posteo.de>
2022-06-01 22:35:28 +0200

    update simulation workflow

--- sim/ghdl/riscv/Manifest.py
+++ sim/ghdl/riscv/Manifest.py
@@ -2,7 +2,7 @@ action = "simulation"
 sim_tool = "ghdl"
 sim_top = "riscv_tb"
 
-sim_post_cmd = "ghdl -r riscv_tb --stop-time=6us --wave=riscv_tb.ghw; gtkwave riscv_tb.ghw"
+sim_post_cmd = "ghdl -r riscv_tb --stop-time=6us --wave=riscv_tb.ghw"
 
 modules = {
     "local": ["../../../testbench/riscv_tb"]
