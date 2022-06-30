87af798bb93526db6a5ec94278014290f0821aec
Benedikt Bauer <benedikt.bauer@posteo.de>
2022-07-01 01:42:15 +0200

    remove ghdl integration

--- sim/ghdl/riscv/Manifest.py
+++
@@ -1,9 +0,0 @@
-action = "simulation"
-sim_tool = "ghdl"
-sim_top = "riscv_tb"
-
-sim_post_cmd = "ghdl -r riscv_tb --stop-time=6us --wave=riscv_tb.ghw"
-
-modules = {
-    "local": ["../../../testbench/riscv_tb"]
-}
